8c0379e7d8c6f4e0bb7a7ea1bc04065edc3186af
salman1136 <salman.shaikh@varthana.com>
2023-07-29 16:57:04 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./components/Homepage"
 
 function App() {
   return (
-    <div className="App">
-     hello
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage/>}> 
+       
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
+ 
 export default App;
